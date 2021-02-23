89d68306c055289bf17802006fe0cadf23885c27
Stuart Douglas <stuart.w.douglas@gmail.com>
2021-02-23 16:58:44 +1100

    Convert a test

--- src/test/java/org/springframework/samples/petclinic/rest/UserRestControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/rest/UserRestControllerTests.java
@@ -1,50 +1,24 @@
 package org.springframework.samples.petclinic.rest;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.security.TestSecurity;
+import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.samples.petclinic.model.User;
-import org.springframework.samples.petclinic.service.clinicService.ApplicationTestConfig;
-import org.springframework.samples.petclinic.service.UserService;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.RestAssured;
 
-@SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = ApplicationTestConfig.class)
-@WebAppConfiguration
+@QuarkusTest
+@TestHTTPEndpoint(UserRestController.class)
 public class UserRestControllerTests {
 
-    @Mock
-    private UserService userService;
-
-    @Autowired
-    private UserRestController userRestController;
-
-    private MockMvc mockMvc;
-
-    @Before
-    public void initVets() {
-        this.mockMvc = MockMvcBuilders.standaloneSetup(userRestController)
-            .setControllerAdvice(new ExceptionControllerAdvice()).build();
-    }
 
     @Test
-    @WithMockUser(roles="ADMIN")
+    @TestSecurity(roles="ADMIN")
     public void testCreateUserSuccess() throws Exception {
         User user = new User();
         user.setUsername("username");
@@ -53,13 +27,12 @@ public class UserRestControllerTests {
         user.addRole( "OWNER_ADMIN" );
         ObjectMapper mapper = new ObjectMapper();
         String newVetAsJSON = mapper.writeValueAsString(user);
-        this.mockMvc.perform(post("/api/users/")
-            .content(newVetAsJSON).accept(MediaType.APPLICATION_JSON_VALUE).contentType(MediaType.APPLICATION_JSON_VALUE))
-            .andExpect(status().isCreated());
+        RestAssured.with().contentType(MediaType.APPLICATION_JSON_VALUE).body(newVetAsJSON).post()
+            .then().statusCode(HttpResponseStatus.CREATED.code());
     }
 
     @Test
-    @WithMockUser(roles="ADMIN")
+    @TestSecurity(roles="ADMIN")
     public void testCreateUserError() throws Exception {
         User user = new User();
         user.setUsername("username");
@@ -67,8 +40,7 @@ public class UserRestControllerTests {
         user.setEnabled(true);
         ObjectMapper mapper = new ObjectMapper();
         String newVetAsJSON = mapper.writeValueAsString(user);
-        this.mockMvc.perform(post("/api/users/")
-            .content(newVetAsJSON).accept(MediaType.APPLICATION_JSON_VALUE).contentType(MediaType.APPLICATION_JSON_VALUE))
-            .andExpect(status().isBadRequest());
+        RestAssured.with().contentType(MediaType.APPLICATION_JSON_VALUE).body(newVetAsJSON).post()
+            .then().statusCode(HttpResponseStatus.BAD_REQUEST.code());
     }
 }
